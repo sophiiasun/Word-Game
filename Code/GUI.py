f49c314d7c8adad677e8d02d81ebe2c68b7cd7ce
tmaswang69 <thomaswang003@gmail.com>
2021-07-02 16:28:18 -0400

    Fixed Image

--- Code/GUI.py
+++ Code/GUI.py
@@ -3,7 +3,10 @@ import time
 from tkinter import *
 import math
 from PIL import ImageTk, Image
-
+import base64
+import urllib
+import urllib.request
+import os
 window = Tk()
 window.resizable(0, 0)
 window.config(padx=10, pady=10)
@@ -16,17 +19,30 @@ description = Label(window, width=138, text="HOW TO PLAY: A prompt will appear o
     Think of a word with those letters (consecutively) in them and type it in the text box on your side of the screen before the timer runs out! \n\
     Each player gets three lives. Win by being the only player remaining or by using all the letters on the left. Have fun!", font="Courier 10", bg="seashell")
 
-# threeHearts = "C:/Users/14168/Documents/GitHub/Word-Game/ThreeHearts.jpeg"
-# twoHearts = "C:/Users/14168/Documents/GitHub/Word-Game/TwoHearts.jpeg"
-# oneHeart = "C:/Users/14168/Documents/GitHub/Word-Game/OneHeart.jpeg"
-# die = "C:/Users/14168/Documents/GitHub/Word-Game/Die.jpeg"
-#
-# img = ImageTk.PhotoImage(Image.open(threeHearts))
-#
-# p1Label = Label(window, width=15, image=img)
+threeHearts = "https://i.ibb.co/qrkbDGT/Drawing-1.png"
+twoHearts = "https://i.ibb.co/j6t62c5/Drawing-2.png"
+oneHeart = "https://i.ibb.co/d02Q6Nw/Drawing-3.png"
+die = "https://i.ibb.co/h9gYVLq/Drawing-4.png"
 
-title.grid(row=1, column=1, columnspan=2)
-description.grid(row=2, column=1, columnspan=2)
+# u = urllib.urlopen(threeHearts)
+# raw_data = u.read()
+# u.close()
+# b64_data = base64.encodestring(raw_data)
+# img = ImageTk.PhotoImage(Image.open(twoHearts))
+urllib.request.urlretrieve(threeHearts, "threeHearts.png")
+urllib.request.urlretrieve(twoHearts, "twoHearts.png")
+urllib.request.urlretrieve(oneHeart, "oneHeart.png")
+urllib.request.urlretrieve(die, "die.png")
+img1 = Image.open("twoHearts.png")
+img2 = img1.resize((230, 100))
 
+img = ImageTk.PhotoImage(img2)
 
-window.update()
+p1Label = Label(window, width=1000, image=img)
+p1Label.grid(row=3, column =1, columnspan = 100, rowspan = 100)
+
+title.grid(row=1, column=1, columnspan=3)
+description.grid(row=2, column=1, columnspan=3)
+
+window.mainloop()
+
