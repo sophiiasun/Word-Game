5d223c9384e518110bd0494d57ea83d67d7a696e
sophiiasun <53122458+sophiiasun@users.noreply.github.com>
2021-07-03 00:03:27 -0400

    Fixed final (hopefully) bugs of images.

--- GUI.py
+++ GUI.py
@@ -12,62 +12,65 @@ window = Tk()
 window.resizable(0, 0)
 window.config(padx=10, pady=10, bg="SlateGray1")
 
-threeHeartsUrl = "https://i.ibb.co/qrkbDGT/Drawing-1.png"
-twoHeartsUrl = "https://i.ibb.co/j6t62c5/Drawing-2.png"
-oneHeartUrl = "https://i.ibb.co/d02Q6Nw/Drawing-3.png"
-dieUrl = "https://i.ibb.co/h9gYVLq/Drawing-4.png"
-pigUrl = "https://i.ibb.co/pPRbw8q/OinkOink.png"
-cowUrl = "https://i.ibb.co/ggg6g8C/MooMoo.png"
-beeUrl = "https://i.ibb.co/K0H5J3H/BeeIcon.png"
-chickenUrl = "https://i.ibb.co/tHMckNz/BakBak.png"
-dogUrl = "https://i.ibb.co/b15XgKd/BarkBark.png"
-urllib.request.urlretrieve(threeHeartsUrl, "threeHearts.png")
-urllib.request.urlretrieve(twoHeartsUrl, "twoHearts.png")
-urllib.request.urlretrieve(oneHeartUrl, "oneHeart.png")
-urllib.request.urlretrieve(dieUrl, "die.png")
-urllib.request.urlretrieve(pigUrl, "pig.png")
-urllib.request.urlretrieve(cowUrl, "cow.png")
-urllib.request.urlretrieve(beeUrl, "bee.png")
-urllib.request.urlretrieve(chickenUrl, "chicken.png")
-urllib.request.urlretrieve(dogUrl, "dog.png")
-
+LIVES_URL = ["https://i.ibb.co/h9gYVLq/Drawing-4.png", "https://i.ibb.co/d02Q6Nw/Drawing-3.png", "https://i.ibb.co/j6t62c5/Drawing-2.png",\
+             "https://i.ibb.co/qrkbDGT/Drawing-1.png"] # Order of lives: 0, 1, 2, 3
+LIVES_PNG = ["die.png", "oneHeart.png", "twoHearts.png", "threeHearts.png"]
+ICONS_URL = ["https://i.ibb.co/pPRbw8q/OinkOink.png", "https://i.ibb.co/ggg6g8C/MooMoo.png", "https://i.ibb.co/K0H5J3H/BeeIcon.png", \
+             "https://i.ibb.co/tHMckNz/BakBak.png", "https://i.ibb.co/b15XgKd/BarkBark.png"]
+ICONS_PNG = ["pig.png", "cow.png", "bee.png", "chicken.png", "dog.png"]
 
 # ============================================================= METHODS =============================================================
 
-def displayLives(num):
-    img = Image.open("die.png")
-    if num == 1:
-        img = Image.open("oneHeart.png")
-    elif num == 2:
-        img = Image.open("twoHearts.png")
-    elif num == 3:
-        img = Image.open("threeHearts.png")
+def requestURLs():
+    for idx in range(0, 4):
+        urllib.request.urlretrieve(LIVES_URL[idx], LIVES_PNG[idx])
+    for idx in range(0, 5):
+        urllib.request.urlretrieve(ICONS_URL[idx], ICONS_PNG[idx])
+requestURLs()
+
+def displayLives(lives):
+    img = Image.open(LIVES_PNG[lives])
     img1 = img.resize((230, 100))
     img2 = ImageTk.PhotoImage(img1)
     return img2
 
 def displayIcons():
-    img = 
+    idx1 = random.randint(0, 4)
+    idx2 = random.randint(0, 4)
+    while idx2 == idx1:
+        idx2 = random.randint(0, 4)
+    img1_1 = Image.open(ICONS_PNG[idx1])
+    img1_2 = img1_1.resize((230, 230))
+    img1_3 = ImageTk.PhotoImage(img1_2)
+    img2_1 = Image.open(ICONS_PNG[idx2])
+    img2_2 = img2_1.resize((230, 230))
+    img2_3 = ImageTk.PhotoImage(img2_2)
+    return [img1_3, img2_3]
 
 # ========================================================== GUI INTERFACE ==========================================================
 
 title = Label(window, width=35, text="Wurd Game", font="Courier 80 bold", bg="seashell")
-description = Label(window, width=107, text="HOW TO PLAY: A prompt will appear on your screen. \
-Think of a word with those letters (consecutively) in them and \ntype it in the text box on your side of the screen before \n \
-    the timer runs out! \n\
-    Each player gets three lives. Win by being the only player remaining or by using all the letters on the left. Have fun!",\
-    font="Courier 25", bg="seashell")
+subtitle = Label(window, width=50, text="HOW TO PLAY", font="Courier 20 bold", bg="seashell")
+description = Label(window, width=107, text="A prompt will appear on your screen. \
+Think of a word with those letters (consecutively) in them and \ntype it in the text box on your side of the screen before \
+the timer runs out! Each player gets three lives. \n Win by being the only player remaining or by using all the letters\
+on the left. Have fun!", font="Courier 25", bg="seashell")
 img = displayLives(3)
 p1Lives = Label(window, width=180, image=img, anchor="center")
 p2Lives = Label(window, width=180, image=img, anchor="center")
-img = displayIcons()
-p1Icon = Label(window, width=180, image=img[0], anchor="center")
-p2Icon = Label(window, width=180, image=img[1], anchor="center")
+img2 = displayIcons()
+p1Icon = Label(window, width=250, image=img2[0], anchor="center")
+p2Icon = Label(window, width=250, image=img2[1], anchor="center")
 
-title.grid(row=1, column=1, columnspan=3)
-description.grid(row=2, column=1, columnspan=3)
-p1Lives.grid(row=3, column=1)
-p2Lives.grid(row=3, column=3)
+title.grid(row=1, column=1, columnspan=3, padx=(0, 10), pady=(0, 10))
+subtitle.grid(row=2, column=1, columnspan=3, padx=(0, 10), pady=(0, 10))
+description.grid(row=3, column=1, columnspan=3, padx=(0, 10), pady=(0, 10))
+p1Lives.grid(row=4, column=1, padx=(0, 10), pady=(0, 10))
+p2Lives.grid(row=4, column=3, padx=(0, 10), pady=(0, 10))
+p1Icon.grid(row=5, column=1, padx=(0, 10), pady=(0, 10))
+p2Icon.grid(row=5, column=3, padx=(0, 10), pady=(0, 10))
+
+
 
 window.mainloop()
 
